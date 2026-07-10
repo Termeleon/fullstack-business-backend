97fd124f89098020c0bab1367cbc5a5aed057bbf
Hua <hua_sijia@outlook.com>
2026-07-10 02:55:40 -0400

    post roles

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
 import { prisma } from "./prismaClient";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   console.log("Request received on root path");
   res.send("Hello World");
+});
+
+app.post('/roles', async (req, res) => {
+  console.log('create role', req.body);
+  try {
+    const role = await prisma.role.create({
+      data: {
+        name: req.body.name,
+        permissions: {
+          connect: req.body.permissions.map((permission: string) => ({name: permission})),
+        }
+      }
+    });
+    res.status(201).json(role);
+  } catch (error) {
+    res.status(409).json({ error: 'Failed to create role' });
+  }
 });
 
 app.get('/roles', async (req, res) => {
@@ -19,6 +39,7 @@ app.get('/roles', async (req, res) => {
   console.log(roles)
   res.status(200).json(roles)
 });
+
 
 
 app.listen(3000, () => {
